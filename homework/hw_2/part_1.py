cd6e54c443a317c7e0c260bf4ef085fe3ae76fc7
Julirym <Rymkojul@gmail.com>
2025-09-01 21:57:25 +0300

    add part_1

--- homework/hw_2/part_1.py
+++ homework/hw_2/part_1.py
@@ -1,3 +1,28 @@
-a = int(input("Число: "))
-kv = a**2
-print("Квадрат числа:", kv)
+# n = int(input("Число: "))
+#
+# if n % 2 == 0:
+#     print("Четное")
+# else:
+#     print("Нечетное")
+#
+# if n % 2 == 0:
+#     print("Четное")
+# else:
+#     print("Нечетное")
+
+
+day = str(input("Сегодня день: "))
+title_day = day.title()
+if title_day == "Суббота" or title_day == "Воскресенье":
+    print("Сегодня выходной!")
+elif title_day == "Среда":
+    print("Мне сегодня к стоматологу в 15:30")
+elif (
+    title_day == "Понедельник"
+    or title_day == "Вторник"
+    or title_day == "Четверг"
+    or title_day == "Пятница"
+):
+    print("Сегодня обычный день")
+else:
+    print("Это не день недели!")
