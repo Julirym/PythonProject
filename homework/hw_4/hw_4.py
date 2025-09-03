288025e5c76ca02e0a1dd659ae00bf33470c2a14
Julirym <Rymkojul@gmail.com>
2025-09-03 13:47:14 +0300

    Тест новой ветки

--- homework/hw_4/hw_4.py
+++ homework/hw_4/hw_4.py
@@ -8,3 +8,14 @@ while True:
     count = count + a  # count += a (тоже самое)
 
 print("Сумма всех введенных чисел (без отрицательного):", count)
+
+# Задача 1 урок 5
+count = 0
+
+while True:
+    a = int(input("Введите целое число: "))
+    if a < 0:
+        break
+    count = count + a  # count += a (тоже самое)
+
+print("Сумма всех введенных чисел (без отрицательного):", count)
