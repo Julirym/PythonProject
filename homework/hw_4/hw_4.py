277306fac2c4d46fab2645dc999da783eb9b47ce
Julirym <Rymkojul@gmail.com>
2025-09-06 22:00:09 +0300

    new tasks

--- homework/hw_4/hw_4.py
+++ homework/hw_4/hw_4.py
@@ -1,97 +1,3 @@
-# Задача 1 урок 3
-# count = 0
-# while True:
-#     a = int(input("Введите целое число: "))
-#     if a < 0:
-#         break
-#     count = count + a  # count += a (тоже самое)
-#
-# print("Сумма всех введенных чисел (без отрицательного):", count)
-from itertools import count
-
-# day = (input("Сегодня день: ")
-# title_day = day.title()
-# if title_day == "Суббота" or title_day == "Воскресенье":
-#     print("Сегодня выходной!")
-# elif title_day == "Среда":
-#     print("Мне сегодня к стоматологу в 15:30")
-# elif (
-#     title_day == "Понедельник"
-#     or title_day == "Вторник"
-#     or title_day == "Четверг"
-#     or title_day == "Пятница"
-# ):
-#     print("Сегодня обычный день")
-# else:
-#     print("Это не день недели!")
-
-#1)Создайте переменную word, значение которой определяется через ввод данных с клавиатуры.
-#Программа должна вывести количество гласных и согласных букв в введенной строке.
-#решение:
-# word = input('Введите слово: ')
-# count_1 = 0
-# count_2 = 0
-# for c in word:
-#     if c in "БВГДЖЗЙКЛМНПРСТФХЦЧШЩбвгджзйклмнпрстфхцчшщ":
-#         count_1 += 1
-#     elif c in "АОУЕЁИЮЭЯЫауоиэыяюеё":
-#         count_2 += 1
-# print("Согласных:", count_1)
-# print("Гласных:", count_2)
-
-# word = input('Введите слово: ')
-# count_1 = 0
-# count_2 = 0
-# letters = "АОУЕЁИЮЭЯЫауоиэыяюеё"
-# for c in word:
-#     if c in letters:
-#         count_1 += 1
-#     elif c not in letters:
-#         count_2 += 1
-# print(f"Гласных: {count_1}, Соласных:, {count_2}")
-
-#2)Напишите программу, которая выводит шаблонное приветствие в зависимости от введённого пользователем времени суток.
-# Если время с 00:00 до 06:00 часов нужно вывести “Доброе утро”, если время с 06:00 до 12:00 часов нужно вывести “Добрый день”,
-# если время с 12:00 до 18:00 часов нужно вывести “Добрый вечер”, если время с 18:00 до 00:00 часов нужно вывести “Доброй ночи”
-#решение:
-# time = (input("Введите время в формате ЧЧ:ММ: "))
-#
-# time_1 = "00:00"
-# time_2 = "06:00"
-# time_3 = "12:00"
-# time_4 = "18:00"
-#
-# if time >= time_1 and time < time_2:
-#     print('Доброе утро')
-# elif time >= time_2 and time < time_3:
-#     print('Добрый день')
-# elif time >= time_3 and time < time_4:
-#     print('Добрый вечер')
-# elif time >= time_4 and time < time_1:
-#     print('Доброй ночи')
-# else:
-#     print("Это не время!")
-
-# 3)Создайте переменную num, значение которой определяется через ввод данных с клавиатуры.
-# Программа должна вывести сумму всех чисел до этого числа включительно.
-# Попробуйте решить разными способами.
-# num = int(input('Введите число:'))
-# total = sum(range(1, num + 1))
-# print(f"Сумма чисел от 1 до", num, "равна", total)
-
-# num = int(input("Введите число: "))
-# total = 0
-# for i in range(1, n + 1):
-#     total += i
-# print("Сумма чисел от 1 до", num, "равна", total)
-
-# count = 0
-# while True:
-#     a = int(input("Введите целое число: "))
-#     if a < 0:
-#         break
-#     count = count + a  # count += a (тоже самое)
-
 #4)Напишите программу, которая в последовательности натуральных чисел определяет максимальное число, кратное 5.
 # Программа получает на вход количество чисел в последовательности, а затем сами числа.
 # В последовательности всегда имеется число, кратное 5.
@@ -109,4 +15,184 @@ from itertools import count
 #Пример:
 #21/3
 #Ответ:7
+
+                                              #УРОК 4
+
+# numbers = []
+# print(numbers)
+
+# numbers = [6, 4, 3, 1]
+# a = numbers[2]
+# b = numbers[0]
+#
+# del numbers[2]
+#
+# print(numbers)
+# print(a)
+# print(b)
+
+# numbers = [6, 4, 3, 1, 5, 4, 3, 3, 2, 1]
+#
+# a = numbers[25]
+# print(a)
+
+# numbers = [1, 4, 5]
+#
+# numbers.append(15)
+# numbers.append('Привет')
+#
+# print(numbers)
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# a = numbers.pop(3)
+#
+# print(numbers)
+# print(a)
+
+# numbers = [5, 12, 1, 5, 3, 2, 'hello', 123]
+# print(len(numbers))
+# print(sorted(numbers))
+# print(sum(numbers))
+# print(min(numbers))
+# print(max(numbers))
+
+# '123', range(10), [1, 2, 3]
+
+# 123 -> int
+# 3.14 -> float
+# 'hello' -> str
+# True -> bool
+# None -> NoneType
+# [1, 2, 3] -> list
+
+# numbers = [5, 12, '123', 1, 5, 3, 2, True]
+
+# for i in numbers:
+#     if type(i) == int:
+#         print(i)
+#
+#
+# for i in numbers:
+#     if isinstance(i, int):
+#         print(i)
+
+# items = [1, '123', 34, True]
+# numbers = []
+#
+# for item in items:
+#     if type(item) == int:
+#         numbers.append(item)
+#
+# print(numbers)
+
+# numbers = []
+#
+# for i in range(10):
+#     if i % 2 == 0:
+#         numbers.append(i)
+#
+# print(numbers)
+#
+# numbers2 = [i for i in range(10) if i % 2 == 0]
+#
+# print(numbers2)
+
+# 123, '123', True, None, ...
+# [1, 2, 3], ...
+
+# a = 5
+# b = a # 5
+#
+# a += 1
+#
+# print(a)
+# print(b)
+#
+# numbers = [1, 2, 3, 4, 5]
+# numbers2 = numbers
+#
+# numbers.append(6)
+#
+# print(numbers)
+# print(numbers2)
+
+# numbers = (1, 2, 3, 4)
+# numbers2 = (1, 2, 3, 4)
+#
+# num3 = numbers + numbers2
+#
+# print(num3)
+#
+#
+# for i in numbers:
+#     print(i)
+#
+# print(numbers)
+
+# print(list('123123'))
+
+# info = [25, 5442433, 'Оксана']
+# info2 = [45, 512323, 'Сережа']
+
+# data = {'name': 'Oksana', 'age': 25, 'id': 244354}
+#
+# info = {'name': 'Сережа', 'country': 'USA'}
+# info['id'] = 324545
+# info['name'] = 'Маша'
+#
+# print(info)
+
+# data = {
+#     'name': 'Oksana',
+#     'age': 25,
+#     'id': 244354,
+#     'country': 'USA'
+# }
+
+# print(data.keys())
+# print(data.values())
+# print(data.items())
+
+# for key in data.items():
+#     print(key)
+
+# for key in data:
+#     data[key] = 123
+#
+# print(data)
+
+# print(data.get('country', 'Страна не указана'))
+# print('country' in data)
+
+# items = [1, 2, 3, {'name': 'Oksana'}, ['a', 'b', 'c']]
+# info = items[3] # {'name': 'Oksana'}
+# letters = items[4] # ['a', 'b', 'c']
+#
+# print(letters[2])
+# print(info)
+# print(items)
+
+# items = [1, 2, 3, {'name': 'Oksana'}, [[1, 2, 3], 'b', 'c']]
+# print(items[3]['name'])
+# print(items[4][0][1])
+
+# data = {
+#     'name': 'Oksana',
+#     'data': {
+#         'id': 2,
+#         'country': 'China',
+#     }
+# }
+#
+# print(data['name'])
+# print(data['data']['id'])
+
+# numbers = [1, 2, 2, 2, 3, 5, 5, 6]
+# result = []
+#
+# for i in numbers:
+#     if i not in result:
+#         result.append(i)
+#
+# print(result)
 
